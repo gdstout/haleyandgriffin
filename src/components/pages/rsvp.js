ab76a5fe3f1e6c6800aa3675ef7d1acddb1c3d5e
gdstout <gdstout27@gmail.com>
2023-04-06 20:08:29 -0400

    rsvp localstorage conditions

--- src/components/pages/rsvp.js
+++ src/components/pages/rsvp.js
@@ -1,15 +1,19 @@
 import {
   Button,
+  CircularProgress,
   Container,
   Grid,
   TextField,
   ToggleButton,
   ToggleButtonGroup,
+  Tooltip,
   Typography,
 } from "@mui/material";
 import { styled } from "@mui/system";
 import React, { useState } from "react";
 import emailjs from "@emailjs/browser";
+import MarkEmailReadTwoToneIcon from "@mui/icons-material/MarkEmailReadTwoTone";
+import CancelScheduleSendTwoToneIcon from "@mui/icons-material/CancelScheduleSendTwoTone";
 
 const formSinglePassword = "solo";
 const formPlusOnePassword = "plus1";
@@ -22,7 +26,10 @@ const StyledGridItem = styled(Grid)({
   maxHeight: "65px",
 });
 
-const StyledField = styled(TextField)({});
+const SmallTypography = styled(Typography)({
+  fontSize: "12px",
+  textDecoration: "underline"
+})
 
 const RSVP = () => {
   const [password, setPassword] = useState("");
@@ -36,6 +43,11 @@ const RSVP = () => {
   const [plusOne, setPlusOne] = useState("No");
   const [plusOneName, setPlusOneName] = useState("");
   const [dietaryRestrictions, setDietaryRestrictions] = useState("");
+
+  const [inSubmission, setInSubmission] = useState(false);
+  //0 = not submitted yet, 1 = success, 2 = failure
+  const [submitResult, setSubmitResult] = useState(JSON.parse(localStorage.getItem('has-rsvpd')) || 0);
+  const [failedSubmissions, setFailedSubmissions] = useState(0);
 
   const handlePasswordInput = (e) => {
     setPassword(e.target.value);
@@ -63,7 +75,7 @@ const RSVP = () => {
 
   const handleEmail = (e) => {
     setEmail(e.target.value);
-  }
+  };
 
   const handleResponse = (e) => {
     setResponse(e.target.value);
@@ -107,6 +119,8 @@ const RSVP = () => {
 
   const submit = (e) => {
     e.preventDefault();
+    setInSubmission(true);
+    setSubmitResult(0);
 
     const template = {
       name: name,
@@ -127,9 +141,15 @@ const RSVP = () => {
       .then(
         function (response) {
           console.log("Success", response.status, response.text);
+          setSubmitResult(1);
+          setInSubmission(false);
+          localStorage.setItem('has-rsvpd', JSON.stringify(1));
         },
         function (error) {
           console.log("Failure", error);
+          setSubmitResult(2);
+          setInSubmission(false);
+          setFailedSubmissions(failedSubmissions + 1);
         }
       );
   };
@@ -148,109 +168,145 @@ const RSVP = () => {
             to RSVP.
           </Typography>
         </Grid>
-        <Grid item>
-          <TextField
-            fullWidth
-            label="Password"
-            variant="outlined"
-            color="secondary"
-            value={password}
-            onChange={handlePasswordInput}
-          />
-        </Grid>
+        {submitResult !== 1 && (
+          <Grid item>
+            <TextField
+              fullWidth
+              label="Password"
+              variant="outlined"
+              color="secondary"
+              value={password}
+              onChange={handlePasswordInput}
+            />
+          </Grid>
+        )}
       </Grid>
-      {!locked && (
-        <FormGrid container direction="column" spacing={2}>
-          <Grid item>
-            <Typography align="center">
-              Great! Now we just need your info.
-            </Typography>
-          </Grid>
-          <Grid item>
-            <TextField
-              fullWidth
-              required
-              label="Your First and Last Name"
-              variant="outlined"
-              color="secondary"
-              value={name}
-              onChange={handleName}
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              fullWidth
-              label="Email (Optional)"
-              variant="outlined"
-              color="secondary"
-              value={email}
-              onChange={handleEmail}
-            />
-          </Grid>
-          <StyledGridItem item>
-            <ToggleButtonGroup
-              value={response}
-              color="secondary"
-              onChange={handleResponse}
-              fullWidth
-            >
-              <ToggleButton value="Accept">I'm Coming!</ToggleButton>
-              <ToggleButton value="Decline">I can't make it</ToggleButton>
-            </ToggleButtonGroup>
-          </StyledGridItem>
-          {response === "Accept" && !plusOneLocked && (
-            <StyledGridItem item>
-              <ToggleButtonGroup
-                value={plusOne}
-                color="secondary"
-                onChange={handlePlusOne}
-                fullWidth
-              >
-                <ToggleButton value="Yes">I'm using my +1</ToggleButton>
-                <ToggleButton value="No">It's just me</ToggleButton>
-              </ToggleButtonGroup>
-            </StyledGridItem>
-          )}
-          {response === "Accept" && plusOne === "Yes" && !plusOneLocked && (
+      <FormGrid container direction="column" spacing={2}>
+        {!locked && submitResult !== 1 && (
+          <>
+            <Grid item>
+              <Typography align="center">
+                Great! Now we just need your info.
+              </Typography>
+            </Grid>
             <Grid item>
               <TextField
                 fullWidth
                 required
-                label="+1's First and Last Name"
-                helperText="We need a name for placecards at the reception!"
+                label="Your First and Last Name"
                 variant="outlined"
                 color="secondary"
-                value={plusOneName}
-                onChange={handlePlusOneName}
+                value={name}
+                onChange={handleName}
               />
             </Grid>
-          )}
-          {response === "Accept" && (
             <Grid item>
               <TextField
                 fullWidth
-                label="Dietary Restrictions"
+                label="Email (Optional)"
                 variant="outlined"
                 color="secondary"
-                value={dietaryRestrictions}
-                onChange={handleDietaryRestrictions}
+                value={email}
+                onChange={handleEmail}
               />
             </Grid>
-          )}
+            <StyledGridItem item>
+              <ToggleButtonGroup
+                value={response}
+                color="secondary"
+                onChange={handleResponse}
+                fullWidth
+              >
+                <ToggleButton value="Accept">I'm Coming!</ToggleButton>
+                <ToggleButton value="Decline">I can't make it</ToggleButton>
+              </ToggleButtonGroup>
+            </StyledGridItem>
+            {response === "Accept" && !plusOneLocked && (
+              <StyledGridItem item>
+                <ToggleButtonGroup
+                  value={plusOne}
+                  color="secondary"
+                  onChange={handlePlusOne}
+                  fullWidth
+                >
+                  <ToggleButton value="Yes">I'm using my +1</ToggleButton>
+                  <ToggleButton value="No">It's just me</ToggleButton>
+                </ToggleButtonGroup>
+              </StyledGridItem>
+            )}
+            {response === "Accept" && plusOne === "Yes" && !plusOneLocked && (
+              <Grid item>
+                <TextField
+                  fullWidth
+                  required
+                  label="+1's First and Last Name"
+                  helperText="We need a name for placecards at the reception!"
+                  variant="outlined"
+                  color="secondary"
+                  value={plusOneName}
+                  onChange={handlePlusOneName}
+                />
+              </Grid>
+            )}
+            {response === "Accept" && (
+              <Grid item>
+                <TextField
+                  fullWidth
+                  label="Dietary Restrictions"
+                  variant="outlined"
+                  color="secondary"
+                  value={dietaryRestrictions}
+                  onChange={handleDietaryRestrictions}
+                />
+              </Grid>
+            )}
 
+            <Grid item>
+              <Button
+                fullWidth
+                variant="contained"
+                color="secondary"
+                disabled={!validForm}
+                onClick={submit}
+              >
+                {inSubmission ? <CircularProgress size="1.5rem" /> : "Submit"}
+              </Button>
+            </Grid>
+          </>
+        )}
+        {submitResult !== 0 && (
           <Grid item>
-            <Button
-              fullWidth
-              variant="contained"
-              color="secondary"
-              disabled={!validForm}
-              onClick={submit}
-            >
-              Submit
-            </Button>
+            {submitResult === 1 && (
+              <>
+                <Typography align="center" color="secondary">
+                  <MarkEmailReadTwoToneIcon
+                    color="secondary"
+                    fontSize="large"
+                  />
+                  <br />
+                  We've received your RSVP! Thank you.
+                  <br />
+                  <Tooltip title="If you need to alter your RSVP, please contact us at rsvphaleyandgriffin@gmail.com" arrow>
+                    <SmallTypography>Help! I've made a mistake!</SmallTypography>
+                  </Tooltip>
+                </Typography>
+              </>
+            )}
+            {submitResult === 2 && (
+              <>
+              <Typography align="center" color="error">
+                <CancelScheduleSendTwoToneIcon color="error" fontSize="large" />
+                <br />
+                Something went wrong, please try again in a few moments.
+              </Typography>
+              {failedSubmissions > 1 && (
+                <Typography align="center">If this error persists, please reach out or manually RSVP at rsvphaleyandgriffin@gmail.com</Typography>
+              )}
+              </>
+            )}
           </Grid>
-        </FormGrid>
-      )}
+        )}
+      </FormGrid>
     </Container>
   );
   return content;

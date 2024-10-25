68a76f162bd68d46b8dab2aa5ab9d1003ea153b9
gdstout <gdstout27@gmail.com>
2024-10-25 17:05:17 -0400

    finalize bet calculations

--- src/components/pages/golf.js
+++ src/components/pages/golf.js
@@ -2,6 +2,7 @@ import {
   Button,
   Container,
   Grid,
+  InputAdornment,
   TextField,
   ToggleButton,
   ToggleButtonGroup,
@@ -9,24 +10,35 @@ import {
 } from "@mui/material";
 import { useEffect, useState } from "react";
 
-const initialPlayers = [{ name: "" }, { name: "" }];
+const initialPlayers = [
+  { name: "", id: 1, holesWonO: [], holesLost: [] },
+  { name: "", id: 2, holesWonO: [], holesLost: [] },
+];
 
 const Golf = () => {
   // global states
   const [page, setPage] = useState(0);
 
-  // setup page status (0)
+  // setup page states (0)
   const [numHoles, setNumHoles] = useState("18");
   const [players, setPlayers] = useState(initialPlayers);
   const [canStart, setCanStart] = useState(false);
+  const [bet, setBet] = useState(4.0);
 
-  // hole page states (1)
+  // hole states (1)
   const [currentHole, setCurrentHole] = useState(0);
   const [scores, setScores] = useState([]);
-  // results page states (2)
+
+  // results states (2)
+  const [payoutMatrix, setPayoutMatrix] = useState([[], []]);
+  const [holeResults, setHoleResults] = useState([]);
 
   const handleNumHolesChange = (e) => {
     setNumHoles(e.target.value);
+  };
+
+  const handleBetChange = (e) => {
+    setBet(parseFloat(e.target.value));
   };
 
   const handleNumPlayersChange = (e) => {
@@ -35,7 +47,12 @@ const Golf = () => {
       newPlayers.pop();
     }
     if (e.target.value === "+") {
-      newPlayers.push({ name: "" });
+      newPlayers.push({
+        name: "",
+        id: players.length + 1,
+        holesWon: [],
+        holesLost: [],
+      });
     }
     setPlayers(newPlayers);
   };
@@ -46,45 +63,148 @@ const Golf = () => {
     setPlayers(newPlayers);
   };
 
-  const checkCanStart = () => {
-    setCanStart(true);
-    players.forEach((player) => {
-      if (player.name.length < 1) {
-        setCanStart(false);
-      }
-    });
-  };
-
+  // monitor canStart
   useEffect(() => {
-    checkCanStart();
-  }, [players]);
+    if (page === 0) {
+      setCanStart(true);
+      players.forEach((player) => {
+        if (player.name.length < 1) {
+          setCanStart(false);
+        }
+      });
+    }
+  }, [players, page]);
 
   const handlePageChange = (i) => (e) => {
+    // setup scores, resultsArray, and betMatrix
     if (i === 1) {
       const blankScores = Array(parseInt(numHoles))
         .fill()
         .map(() => Array(players.length).fill(4));
-      console.log(blankScores);
       setScores(blankScores);
+
+      const blankPayoutMatrix = Array(players.length)
+        .fill()
+        .map(() => Array(players.length).fill(0));
+      setPayoutMatrix(blankPayoutMatrix);
+
+      const blankHoleResults = Array(parseInt(numHoles)).fill({
+        winners: [],
+        losers: [],
+      });
+      setHoleResults(blankHoleResults);
     }
     setPage(i);
   };
 
-  const handleScoresChange = (currentHole, playerIndex) => (e) => {
+  const handleScoresChange = (currentHole, playerIndex, toggle) => (e) => {
     let newScores = structuredClone(scores);
-    newScores[currentHole][playerIndex] = e.target.value;
+    if (toggle) {
+      if (e.target.value === "-") {
+        newScores[currentHole][playerIndex] =
+          parseInt(newScores[currentHole][playerIndex]) - 1;
+      } else {
+        newScores[currentHole][playerIndex] =
+          parseInt(newScores[currentHole][playerIndex]) + 1;
+      }
+    } else {
+      newScores[currentHole][playerIndex] = parseInt(e.target.value);
+    }
     setScores(newScores);
   };
+
+  // calculate bet payouts every time the score changes
+  useEffect(() => {
+    let newHoleResults = structuredClone(holeResults);
+
+    scores.forEach((holeScores, i) => {
+      newHoleResults[i] = calculateHoleResults(holeScores);
+    });
+    let newPayoutMatrix = updatePayoutMatrix(newHoleResults);
+
+    setHoleResults(newHoleResults);
+    setPayoutMatrix(newPayoutMatrix);
+  }, [scores, currentHole]);
+
+  function calculateHoleResults(holeScores) {
+    let lowestScore = holeScores[0];
+    let highestScore = holeScores[0];
+    let result = { winners: [], losers: [], push: true };
+
+    // find lowest and highest scores or push
+    holeScores.forEach((score, i) => {
+      if (score < lowestScore) {
+        lowestScore = score;
+        result.push = false;
+      }
+      if (score > highestScore) {
+        highestScore = score;
+        result.push = false;
+      }
+    });
+
+    if (result.push) return result;
+
+    // find winners and losers
+    holeScores.forEach((score, i) => {
+      if (score === lowestScore) {
+        result.winners.push(i);
+      }
+      if (score === highestScore) {
+        result.losers.push(i);
+      }
+    });
+    return result;
+  }
+
+  function updatePayoutMatrix(holeResults) {
+    let tmpPayoutMatrix = Array(players.length)
+      .fill()
+      .map(() => Array(players.length).fill(0));
+    // add up all payments
+    holeResults.forEach((result, i) => {
+      if (i <= currentHole) {
+        result.losers.forEach((loser) => {
+          result.winners.forEach((winner) => {
+            tmpPayoutMatrix[loser][winner] =
+              tmpPayoutMatrix[loser][winner] +
+              bet / result.winners.length / result.losers.length;
+          });
+        });
+      }
+    });
+
+    // eliminate payments across
+    for (let i = 0; i < players.length; i++) {
+      for (let j = 0; j < players.length; j++) {
+        if (i === j) {
+          //continue;
+        }
+        if (tmpPayoutMatrix[i][j] >= tmpPayoutMatrix[j][i]) {
+          tmpPayoutMatrix[i][j] = tmpPayoutMatrix[i][j] - tmpPayoutMatrix[j][i];
+          tmpPayoutMatrix[j][i] = 0;
+        }
+      }
+    }
+
+    return tmpPayoutMatrix;
+  }
 
   const handleNext = () => {
     if (currentHole < parseInt(numHoles) - 1) {
       setCurrentHole(currentHole + 1);
+    } else {
+      setPage(2);
     }
   };
 
   const handlePrevious = () => {
     if (currentHole > 0) {
-      setCurrentHole(currentHole - 1);
+      if (page === 2) {
+        setPage(1);
+      } else {
+        setCurrentHole(currentHole - 1);
+      }
     } else {
       setPage(0);
     }
@@ -108,17 +228,44 @@ const Golf = () => {
                 exclusive
                 fullWidth
               >
-                <ToggleButton value="9">9</ToggleButton>
-                <ToggleButton value="18">18</ToggleButton>
+                <ToggleButton color="secondary" value="9">
+                  9
+                </ToggleButton>
+                <ToggleButton color="secondary" value="18">
+                  18
+                </ToggleButton>
               </ToggleButtonGroup>
+            </Grid>
+            <Grid item>
+              <Typography variant="h5">Bet</Typography>
+            </Grid>
+            <Grid item>
+              <TextField
+                fullWidth
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">$</InputAdornment>
+                  ),
+                }}
+                label="Amount"
+                type="number"
+                variant="filled"
+                color="secondary"
+                value={bet}
+                onChange={handleBetChange}
+              ></TextField>
             </Grid>
             <Grid item>
               <Typography variant="h5">Players</Typography>
             </Grid>
             <Grid item>
               <ToggleButtonGroup onChange={handleNumPlayersChange} fullWidth>
-                <ToggleButton value="-">-</ToggleButton>
-                <ToggleButton value="+">+</ToggleButton>
+                <ToggleButton color="secondary" value="-">
+                  -
+                </ToggleButton>
+                <ToggleButton color="secondary" value="+">
+                  +
+                </ToggleButton>
               </ToggleButtonGroup>
             </Grid>
             {players.map((player, i) => (
@@ -154,34 +301,85 @@ const Golf = () => {
             </Grid>
             {scores[currentHole].map((score, i) => (
               <Grid item key={i}>
-                <TextField
-                  type="number"
-                  label={players[i].name}
-                  variant="outlined"
-                  color="secondary"
-                  value={scores[currentHole][i]}
-                  onChange={handleScoresChange(currentHole, i)}
-                ></TextField>
+                <Grid container direction="row" justifyContent="space-between">
+                  <Grid item>
+                    <TextField
+                      type="number"
+                      label={players[i].name}
+                      variant="outlined"
+                      color="secondary"
+                      value={scores[currentHole][i]}
+                      onChange={handleScoresChange(currentHole, i, false)}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <ToggleButtonGroup
+                      onChange={handleScoresChange(currentHole, i, true)}
+                    >
+                      <ToggleButton value="-" size="large">
+                        -
+                      </ToggleButton>
+                      <ToggleButton value="+" size="large">
+                        +
+                      </ToggleButton>
+                    </ToggleButtonGroup>
+                  </Grid>
+                </Grid>
               </Grid>
             ))}
+          </>
+        )}
+        <Grid item>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            {page > 0 && (
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={handlePrevious}
+              >
+                Back
+              </Button>
+            )}
+            {page === 1 && (
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={handleNext}
+              >
+                Next Hole
+              </Button>
+            )}
+          </Grid>
+        </Grid>
+        {page > 0 && (
+          <>
             <Grid item>
-              <Grid container direction="row" justifyContent="space-between">
-                <Button
-                  variant="outlined"
-                  color="secondary"
-                  onClick={handlePrevious}
-                >
-                  Back
-                </Button>
-                <Button
-                  variant="contained"
-                  color="secondary"
-                  onClick={handleNext}
-                >
-                  Next Hole
-                </Button>
-              </Grid>
+              <Typography variant="h5">Results</Typography>
             </Grid>
+            {payoutMatrix.map((loser, i) => (
+              <>
+                {loser.map((amount, j) => (
+                  <>
+                    {i !== j && amount > 0 ? (
+                      <Grid item key={`${players[i].id} yeet ${players[j].id}`}>
+                        <Typography
+                          key={`${players[i].id} yeet2 ${players[j].id}`}
+                        >
+                          {players[i].name} owes {players[j].name} ${amount}
+                        </Typography>
+                      </Grid>
+                    ) : (
+                      <></>
+                    )}
+                  </>
+                ))}
+              </>
+            ))}
           </>
         )}
       </Grid>
